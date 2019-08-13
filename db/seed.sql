4b22279779a89b6efdf87e657ca1f589416846d0
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-13 17:31:04 -0400

    added basic files and folder and hooked up to JawsDB

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("The Original Chubby Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("The Spicy Habanero Chicken Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("BLT Chubby Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Buddy Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Clubhouse Burger");
