1fe151e3328b5fc052698bb416c079b4fabbd6c8
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-14 19:58:20 -0400

    fixed devour issue

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,40 @@
+$(function() {
+  $(".devoure").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var newBurgerState = {
+      devoured: true,
+    };
+
+    console.log(newBurgerState);
+    console.log(id);
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState,
+    }).then(function() {
+      console.log("Changed state to", newBurgerState);
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+    var burgName = $("#ca")
+      .val()
+      .trim();
+    console.log("The burger to insert is :" + burgName);
+    var newBurger = {
+      burger_name: burgName,
+      // devoured: false
+    };
+    console.log(newBurger);
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function() {
+      console.log("New Burger created");
+      location.reload();
+    });
+  });
+});
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 
+var bodyParser = require("body-parser");
+
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -8,8 +10,8 @@ var app = express();
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -22,8 +24,7 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+// Starts server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
